609547384ac8820133b77c65ff576e456b985f71
david <david.tsaturyan.95@gmail.com>
2021-10-30 09:30:31 +0400

    simple recursive mutex deadlock example

---
+++ multithreading/deadlock_examples/single_deadlock_example/deadlock_example/Source.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <thread>
+#include <future>
+
+std::recursive_mutex m;
+
+void print_args() {
+	std::cout << "Не будет вызываться, но мы должны были определить эту функцию" << ' ';
+}
+
+template <typename FirstArg, typename ...RestArgs>
+void print_args(FirstArg t, RestArgs&&... args) {
+	m.lock();
+	constexpr std::size_t num_args = sizeof...(args);
+	if (num_args == 0) {
+		std::cout << t << ' ';
+		m.unlock();
+		return;
+	}
+
+	std::cout << t << ' ';
+	print_args(std::forward<RestArgs>(args)...);
+
+	m.unlock();
+}
+
+
+int main() {
+
+	
+	print_args("David", 26, 'M', "Daria", 23);
+
+	return 0;
+}
