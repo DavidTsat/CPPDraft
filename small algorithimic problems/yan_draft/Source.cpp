3b60f31b2c4d6bd0ab5c42604b5a5c62837f7e6e
david <david.tsaturyan.95@gmail.com>
2021-11-18 20:32:17 +0400

    one more problem

--- small algorithimic problems/yan_draft/Source.cpp
+++ small algorithimic problems/yan_draft/Source.cpp
@@ -385,6 +385,63 @@ public:
 	}
 };
 
+/*
+Given a string s, find the length of the longest substring without repeating characters.
+
+
+
+Example 1:
+
+Input: s = "abcabcbb"
+Output : 3
+Explanation : The answer is "abc", with the length of 3.
+Example 2 :
+
+Input : s = "bbbbb"
+Output : 1
+Explanation : The answer is "b", with the length of 1.
+Example 3 :
+
+Input : s = "pwwkew"
+Output : 3
+Explanation : The answer is "wke", with the length of 3.
+Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+Example 4 :
+
+Input : s = ""
+Output : 0
+
+
+*/
+class Solution {
+	bool contains(std::string::const_iterator b, std::string::const_iterator e, char v) {
+		while (b != e) {
+			if (*b++ == v) {
+				return true;
+			}
+		}
+		return false;
+	}
+public:
+	int lengthOfLongestSubstring(string s) {
+		if (s.empty()) return 0;
+		int max_ = 1;
+
+		for (auto i = s.cbegin(); i != s.cend(); ++i) {
+			//  auto i_copy_b = i;
+			auto i_copy_c = i;
+			int temp_max = 0;
+			// std::find(i, i_copy_c, *++i_copy_c) != i_copy_c
+			while (i_copy_c != s.cend() && !contains(i, i_copy_c, *i_copy_c++)) {
+				++temp_max;
+			}
+			if (temp_max > max_) {
+				max_ = temp_max;
+			}
+		}
+		return max_;
+	}
+};
 int main() {
 	std::vector<std::string> all_pars = generate_pars(4);
 	std::cout << std::endl;
