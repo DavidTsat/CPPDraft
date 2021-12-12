741319c0cfdb34c2a3f971aef23993516708ad49
david <david.tsaturyan.95@gmail.com>
2021-12-12 17:05:02 +0400

    some changes on prev problem

--- algorithimic problems/dynamic programming/best-time-to-buy-and-sell-stock-ii/Source.cpp
+++ algorithimic problems/dynamic programming/best-time-to-buy-and-sell-stock-ii/Source.cpp
@@ -3,29 +3,22 @@
 #include <iostream>
 
 using namespace std;
-
 class Solution {
 public:
     int maxProfit(const vector<int>& prices) {
-        int max_ = prices[0];
-        int cur = prices[0];
         int max_prof = 0;
-        int curr_prof = 0;
+        int prev = prices[0];
 
         for (int i = 1; i < prices.size(); ++i)
         {
-            max_ = max(max_, prices[i]);
-            curr_prof = max(curr_prof, max_ - cur);
-            if (prices[i] < max_)
-            {
-                cur = max_ = prices[i];
-                max_prof += curr_prof;
-                curr_prof = 0;
-            }
+            int max_ = max(prev, prices[i]);
+            int curr_prof = max_ - prev;
+            prev = prices[i];
 
+            max_prof += curr_prof;
         }
 
-        return max_prof + curr_prof;
+        return max_prof;
     }
 };
 
