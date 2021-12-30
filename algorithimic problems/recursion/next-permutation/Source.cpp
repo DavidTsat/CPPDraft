2063ac732c489ebeb603d3467d171f2b47830ebe
david <david.tsaturyan.95@gmail.com>
2021-12-30 15:41:14 +0400

    nextP refactored & added iterators support

--- algorithimic problems/recursion/next-permutation/Source.cpp
+++ algorithimic problems/recursion/next-permutation/Source.cpp
@@ -4,6 +4,7 @@
 
 using namespace std;
 
+//vector<int> v({ 1,3,2 });
 class Solution {
     void nextP(vector<int>& nums) {
         int i = nums.size() - 1;
@@ -27,21 +28,52 @@ class Solution {
         reverse(nums.begin() + max(0, i + 1), nums.end());
     }
 
+    template <typename It>
+    bool nextP(It b, It e) {
+        if (b == e) return false;
+        bool c = false;
+        It f = e;
+        --f;
+        if (b == f) return false;
+        It f2 = f;
+
+        while (*--f2 >= *f-- && f2 != b);
+
+        if (f2 != b || (f2 == b && *f2 < *next(f2)))
+        {
+            c = true;
+            It e2 = e;
+            while (--e2 != f2 && *e2 <= *f2);
+            iter_swap(f2, e2);
+        }
+
+        if (c)
+        {
+            reverse(next(f2), e);
+            return true;
+        }
+        else
+        {
+            reverse(b, e);
+            return false;
+        }
+    }
+
 public:
     void nextPermutation(vector<int>& nums) {
-        nextP(nums);
+        nextP(nums.begin(), nums.end());
     }
 
 };
 
 int main()
 {
-    //  vector<int> v({ 1,3,2 });
-   //   vector<int> v({ 2,1,3 });
+    
+      vector<int> v({ 2,1,3 });
    //    vector<int> v({ 2,3, 1 });
    //   vector<int> v({ 3,1,2 });
    //   vector<int> v({ 3,2,1 });
-    vector<int> v({ 2,1,5,4,6,3,2 });
+  //  vector<int> v({ 2,1,5,4,6,3,2 });
     Solution s;
 
     s.nextPermutation(v);
--- algorithimic problems/recursion/permutations-ii/Source.cpp
+++ algorithimic problems/recursion/permutations-ii/Source.cpp
@@ -7,6 +7,28 @@ using namespace std;
 class Solution {
     vector<vector<int>> r;
 
+    void nextP(vector<int>& nums) {
+        int i = nums.size() - 1;
+        bool b = false;
+
+        for (; i >= 0 && !b; --i)
+        {
+            if (i > 0 && nums[i - 1] < nums[i])
+            {
+                for (int j = nums.size() - 1; j > i - 1; --j)
+                {
+                    if (nums[j] > nums[i - 1])
+                    {
+                        swap(nums[j], nums[i - 1]);
+                        b = true;
+                        break;
+                    }
+                }
+            }
+        }
+        reverse(nums.begin() + max(0, i + 1), nums.end());
+    }
+
     void f_slow(vector<int> nums, int j)
     {
         if (j >= nums.size())
@@ -41,11 +63,49 @@ class Solution {
             f_fast(nums, j + 1);
         }
     }
+
+    template <typename It>
+    bool nextP(It b, It e) {
+        if (b == e) return false;
+        bool c = false;
+        It f = e;
+        --f;
+        if (b == f) return false;
+        It f2 = f;
+
+        while (*--f2 >= *f-- && f2 != b);
+
+        if (f2 != b || (f2 == b && *f2 < *next(f2)))
+        {
+            c = true;
+            It e2 = e;
+            while (--e2 != f2 && *e2 <= *f2);
+            iter_swap(f2, e2);
+        }
+
+        if (c)
+        {
+            reverse(next(f2), e);
+            return true;
+        }
+        else
+        {
+            reverse(b, e);
+            return false;
+        }
+    }
+
 public:
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-
         sort(nums.begin(), nums.end());
-        f_fast(nums, 0);
+        //    f_fast(nums, 0);
+        vector<int> v = nums;
+
+        while (nextP(v.begin(), v.end()))
+        {
+            r.push_back(v);
+        }
+        r.push_back(v);
         return r;
     }
 };
@@ -54,6 +114,7 @@ int main()
 {
     vector<int> v({ 1,2,2,2,3,3,4,5 });
 
+  //  vector<int> v({ 0,-1,1 });
     Solution s;
 
    
