d841853651308581a097d83ebde654c4a9065204
David Tsaturyan <davidt@tce.loc>
2022-01-12 17:29:26 +0400

    longestPalindrome_imrpoved_slow

--- algorithimic problems/dynamic programming/longest-palindromic-substring/Source.cpp
+++ algorithimic problems/dynamic programming/longest-palindromic-substring/Source.cpp
@@ -1,41 +1,53 @@
 #include <iostream>
 #include <string>
-
+#include <iterator>
+#include <set>
+#include <algorithm>
 using namespace std;
 
 class Solution {
     string r;
 
-    bool check_palindrome(string::const_iterator l, string::const_iterator r)
+    pair<bool, bool> check_palindrome(string::const_iterator l, string::const_iterator r)
     {
+        auto l_prev = l;
+        bool same = true;
+
         while (l < r && *l == *r)
         {
             ++l;
             --r;
+            if (same && *l != *l_prev)
+            {
+                same = false;
+            }
+            l_prev = l;
         }
 
-        return l >= r;
+        return { l >= r, same };
     }
 
-    void longest_palindrome_before(const string& s, string::const_iterator itr)
+    pair<pair<bool,bool>, string::const_iterator> longest_palindrome_before(const string& s, string::const_iterator itr)
     {
         auto l = s.cbegin();
 
         while (true)
         {
-
             while (*l != *itr)
             {
                 ++l;
             }
             if (l != itr)
             {
-                bool b = check_palindrome(l, itr);
-                if (b)
+                pair<bool, bool> b = check_palindrome(l, itr);
+                if (b.first)
                 {
-                    string p(l, next(itr));
-                    r = p.size() > r.size() ? p : r;
-                    break;
+                    if (distance(l, next(itr)) >= r.size())
+                    {
+                        string p(l, next(itr));
+                        r = p;
+                    }
+                    return { {true, b.second}, l };
                 }
                 else
                 {
@@ -47,20 +59,89 @@ class Solution {
                 break;
             }
         }
-
+        return { {false, false}, s.cend() };
     }
 
-public:
-    string longestPalindrome(string s) {
-
+    string longestPalindrome_slow(string s) // n^2
+    {
         for (auto it = s.cbegin(); it != s.cend(); ++it)
         {
             longest_palindrome_before(s, it);
-            // r = p.size() > r.size() ? p : r;
         }
 
         return !r.empty() ? r : s.substr(0, 1);
+    }
 
+    string longestPalindrome_imrpoved_slow(string s)
+    {
+        pair<pair<bool, bool>, string::const_iterator> p = { {false, false}, s.cbegin() };
+        for (auto it = s.cbegin(); it != s.cend(); ++it)
+        {
+            if (p.first.first == true)
+            {
+                if (p.first.second == true)
+                {
+                    if (*p.second == *it)
+                    {
+                        if (distance(p.second, it) >= r.size())
+                        {
+                            r = string(p.second, next(it));
+
+                            continue;
+                        }
+                    }
+                    else if (p.second != s.cbegin() && *--p.second == *it)
+                    {
+                        if (distance(p.second, it) >= r.size())
+                        {
+                            r = string(p.second, next(it));
+
+                        }
+                        p.first.second = false;
+                        continue;
+                    }
+                    else
+                    {
+                        p = { {false,false}, s.cbegin() };
+                        continue;
+                    }
+                }
+                else
+                {
+                    if (p.second == s.cbegin())
+                    {
+                        p = { {false,false}, s.cbegin() };
+                        continue;
+                    }
+                    else
+                    {
+                        if (*--p.second == *it)
+                        {
+                            if (distance(p.second, it) >= r.size())
+                            {
+                                r = string(p.second, next(it));
+                            }
+                            continue;
+                        }
+                        else
+                        {
+                            p = { {false,false}, s.cbegin() };
+                            continue;
+                        }
+                    }
+                }
+            }
+
+            p = longest_palindrome_before(s, it);
+
+        }
+
+        return !r.empty() ? r : s.substr(0, 1);
+    }
+public:
+    string longestPalindrome(string s) {
+       
+        return longestPalindrome_imrpoved_slow(s);
     }
 };
 
@@ -68,7 +149,12 @@ int main()
 {
     Solution s;
 
-    cout << s.longestPalindrome("aacabdkacaa");
-
+    //cout << s.longestPalindrome("aacabdkacaa"); 
+    // "abcdbbfcba"
+    // "babaddtattarrattatddetartrateedredividerb"
+    //lqlvciwekzxapmjxyddlaoqhfhwphamsyfwjinkfvciucjhdgwodvmnpkibumexvlsxxumvrznuuyqfavmgwfnsvfbrvqhlvhpxaqehsiwxzlvvtxsnmlilbnmvnxyxitxwoahjricdjdncvartepfpdfndxqoozkfpdmlpvshzzffsspdjzlhmamqooooor
+    //azwdzwmwcqzgcobeeiphemqbjtxzwkhiqpbrprocbppbxrnsxnwgikiaqutwpftbiinlnpyqstkiqzbggcsdzzjbrkfmhgtnbujzszxsycmvipjtktpebaafycngqasbbhxaeawwmkjcziybxowkaibqnndcjbsoehtamhspnidjylyisiaewmypfyiqtwlmejkpzlieolfdjnxntonnzfgcqlcfpoxcwqctalwrgwhvqvtrpwemxhirpgizjffqgntsmvzldpjfijdncexbwtxnmbnoykxshkqbounzrewkpqjxocvaufnhunsmsazgibxedtopnccriwcfzeomsrrangufkjfzipkmwfbmkarnyyrgdsooosgqlkzvorrrsaveuoxjeajvbdpgxlcrtqomliphnlehgrzgwujogxteyulphhuhwyoyvcxqatfkboahfqhjgujcaapoyqtsdqfwnijlkknuralezqmcryvkankszmzpgqutojoyzsnyfwsyeqqzrlhzbc
+    //"abbcccbbbcaaccbababcbcabca"
+    cout << s.longestPalindrome("aacabdkacaa"); // "ddtattarrattatdd" // ooooo
     return 0;
 }
