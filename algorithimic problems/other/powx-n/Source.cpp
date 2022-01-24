295f270d9a577b5c2b0e152d30b452b09dedbecd
David Tsaturyan <davidt@tce.loc>
2022-01-24 16:45:28 +0400

    reduced

--- algorithimic problems/other/powx-n/Source.cpp
+++ algorithimic problems/other/powx-n/Source.cpp
@@ -5,77 +5,21 @@
 using namespace std;
 
 class Solution {
-
-    double pow_(vector<double>& v, double a, int k)
+    double pow_(double a, double k)
     {
-        if (k == 0)
-        {
-            return 1;
-        }
-        if (k == 1)
-        {
-            return a;
-        }
-        if (v[abs(k)] != -1) return v[k];
-
-        v[abs(k/2)] = pow_(v, a, abs(k / 2));
-        double r = v[abs(k/2)];
-
-        if ((int)k % 2 == 0)
-        {
-            r *= r;
-        }
-        else
-        {
-            r = a * r * r;
-        }
-
-        if (k > 0)
-        {
-            return r;
-        }
-        else
-        {
-            return (double)(1 / r);
-        }
-    }
-
-
-    double pow_(double a, int k)
-    {
-        if (k == 0)
-        {
-            return 1;
-        }
-        if (k == 1)
-        {
-            return a;
-        }
+        if (k == 0)  return 1;
+        if (k == 1)  return a;
 
         double r = pow_(a, abs(k / 2));
 
-        if ((int)k % 2 == 0)
-        {
-            r *= r;
-        }
-        else
-        {
-            r = a * r * r;
-        }
+        if ((int)k % 2 == 0)  r *= r;
+        else r = a * r * r;
 
-        if (k > 0)
-        {
-            return r;
-        }
-        else
-        {
-            return (double)(1 / r);
-        }
+        return k > 0 ? r : 1 / r;
     }
 public:
-    double myPow(double x, int n) {
-        vector<double> v(abs(n)+1, -1);
-        return pow_(v, x, n);
+    double myPow(double x, double n) {
+        return pow_(x, n);
     }
 };
 
