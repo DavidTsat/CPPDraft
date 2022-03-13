78ab5ea422c908943977169b3220cbd42c42484e
davidtsat <david.tsaturyan.95@gmail.com>
2022-03-13 16:04:33 +0400

    coin-change f_fast

--- algorithimic problems/dynamic programming/coin-change/Source.cpp
+++ algorithimic problems/dynamic programming/coin-change/Source.cpp
@@ -77,15 +77,16 @@ class Solution {
         {
             b = true;
             if (sz_ == 0 || sz < sz_) sz_ = sz;
+            
+            return;
         }
-        if (s >= a)
+        if (s > a)
         {
             s -= l;
             sz -= 1;
             return;
         }
 
-        //for (int i = j; i < c.size(); ++i)
         for (int i = 0; i <= j; ++i)
         {
             s += c[i];
@@ -95,8 +96,30 @@ class Solution {
         sz -= 1;
         s -= l;
     }
+
+    int f_fast(vector<int>& c_, int a)
+    {
+        vector<long long> v(a + 1, INT_MAX);
+        v[0] = 0;
+
+        sort(c_.begin(), c_.end());
+
+        for (int i = 1; i <= a; ++i)
+        {
+            for (int c : c_)
+            {
+                if (c > i) break;
+                if (v[i - c] != INT_MAX)
+                    v[i] = min(v[i], v[i - c] + 1);
+            }
+        }
+        return v[a] == INT_MAX ? -1 : v[a];
+    }
 public:
     int coinChange(vector<int>& coins, int amount) {
+        return f_fast(coins, amount);
+
+        /*
         sort(coins.begin(), coins.end(), greater<int>());
         if (amount == 0) return 0;
         for (int i = 0; i < coins.size(); ++i)
@@ -114,6 +137,7 @@ public:
         return sz_;
         //if (r.empty()) return b ? 0 : -1;
         //return r.size();
+        */
     }
 };
 
@@ -121,17 +145,18 @@ int main()
 {
     vector<int> v({ 1,2,5 }); //11, 100
     vector<int> v2({ 1, 2147483647 }); // 2
-    
-    vector<int> v3({ 186, 419, 83, 408 }); //6249
-    
-    vector<int> v4({ 3, 7, 405, 436 }); // 8839
-    
 
+    vector<int> v3({ 186, 419, 83, 408 }); //6249
+
+    vector<int> v4({ 3, 7, 405, 436 }); // 8839
+
+    vector<int> v5({ 411, 412, 413, 414, 415, 416, 417, 418, 419, 420, 421, 422 }); //9864
+    
     Solution s;
     
   //  cout << accumulate(v2.cbegin(), v2.cend(), (long long)0) << endl;
   //   cout << s.coinChange(v3, 6249) << endl;
-    cout << s.coinChange(v4, 8839) << endl;
+    cout << s.coinChange(v5, 9864) << endl;
 
     return 0;
 }
