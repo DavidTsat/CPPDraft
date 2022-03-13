1a8e74b08d046c6cbecf8bc6698a65996fae76b9
davidtsat <david.tsaturyan.95@gmail.com>
2022-03-13 20:28:01 +0400

    task 4

--- drafts/interview/CPP tasks/Source.cpp
+++ drafts/interview/CPP tasks/Source.cpp
@@ -5,6 +5,7 @@
 #include <vector>
 #include <string>
 #include <map>
+#include <chrono>
 
 // task 1
 void update_count(std::map<std::string, int> storage, std::string str)
@@ -140,12 +141,57 @@ public:
 		std::cout << "c::f()" << std::endl;
 	}
 };
-int main()
+
+void test_task3()
 {
-	C c();
+	C c;// ();
 	//c.f();
 	{
 		C c2(c);
 	}
+}
+
+// task 4. compute pow(x, a)
+
+int cc = 0;
+double p_1(double x, double a)
+{
+	if (a == 0) return 1;
+	if (a == 1) return x;
+	++cc;
+
+	if (a < 0)
+		return 1 / (x * p_1(x, abs(a) - 1));
+
+	return x * p_1(x, abs(a) - 1);
+}
+
+double p_2(double x, double a)
+{
+	if (a == 0) return 1;
+	if (a == 1) return x;
+	++cc;
+
+	double pow_ = p_2(x, abs(a / 2));
+
+	if ((int)a % 2 == 0)
+		pow_ = pow_ * pow_;
+	else
+		pow_ = pow_ * pow_ * x;
+
+	return a < 0 ? 1 / pow_ : pow_;
+}
+
+void test_task4(double (*f_)(double, double))
+{
+	std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+	std::cout << f_(2, -2) << std::endl;
+	std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+	std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << " ms" << std::endl;
+	//std::cout << "CCCCC: " << cc << std::endl;
+}
+int main()
+{
+	test_task4(p_1);
 	return 0;
 }
