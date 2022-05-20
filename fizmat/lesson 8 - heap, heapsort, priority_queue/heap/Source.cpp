5209a5803585eb4f21a8b83e69cf8e0414e54001
davidtsat <david.tsaturyan.95@gmail.com>
2022-05-20 18:22:03 +0400

    heap

--- fizmat/lesson 8 - heap, heapsort, priority_queue/heap/Source.cpp
+++ fizmat/lesson 8 - heap, heapsort, priority_queue/heap/Source.cpp
@@ -11,200 +11,170 @@
 
 using namespace std;
 
-template <typename T, typename Cmp = greater<T>>
+template <typename T, typename Compare = greater<T>>
 class heap;
 
-template <typename T, typename Cont = heap<T>, typename Cmp = greater<T>>
+template<
+	class T,
+	class Compare = std::greater<T>,
+	class Container = heap<T, Compare>
+>
 class priority_queue_;
 
-template <typename T, typename Cmp>
+
+template <typename T, typename Compare>
 class heap
 {
+	friend class priority_queue_<T>;
+public:
 	using value_type = T;
+private:
+	vector<value_type> v;
+	Compare cmp;
+	size_t heap_size;
+	bool is_heap;
 
-	vector<T> v;
-	Cmp cmp;
-	unsigned heap_size;
-
-	friend class priority_queue_<value_type>;
-
-	constexpr unsigned parent(unsigned i) const
+	size_t parent(size_t i) 
 	{
 		return i >> 1;
 	}
-
-	constexpr unsigned left(unsigned i) const
+	size_t left(size_t i)
 	{
 		return i << 1;
 	}
-
-	constexpr unsigned right(unsigned i) const
+	size_t right(size_t i)
 	{
-		return (i << 1) | 1;
+		return i << 1 | 1;
 	}
 
-	constexpr bool is_leaf(unsigned i) const
+	void heapify(size_t i)
 	{
-		return i >= (heap_size / 2);
-	}
+		/*
+			4
+		   / \
+		  -1  2
+		 / \
+		1   0
+		*/
+		size_t l = left(i);
+		size_t r = right(i);
+		size_t largest = i;
 
-	void _heapify_(unsigned i)
-	{
-		unsigned i_max = i;
-		if (left(i) <= heap_size && cmp(v[left(i) - 1], v[i_max - 1]))
+		if (l <= heap_size && cmp(v[l], v[i]))
 		{
-			i_max = left(i);
+			largest = l;
 		}
-		if (right(i) <= heap_size && cmp(v[right(i) - 1], v[i_max - 1]))
+		if (r <= heap_size && cmp(v[r], v[largest]))
 		{
-			i_max = right(i);
+			largest = r;
 		}
-		if (i_max == i) return;
-
-		swap(v[i_max - 1], v[i - 1]);
-		_heapify_(i_max);
+		if (largest == i) return;
+		swap(v[i], v[largest]);
+		heapify(largest);
 	}
 
-	void _build_heap_()
+	void build_heap()
 	{
-		heap_size = v.size();
-		for (int i = heap_size/2; i > 0; --i)
+		heap_size = v.size() - 1;
+		for (int i = heap_size / 2; i >= 1; --i)
 		{
-			_heapify_(i);
+			heapify(i);
 		}
+		is_heap = true;
 	}
 
-	void _heap_increase_val_(unsigned i, const T& new_val)
+	void increase_val(size_t i, const value_type& new_val)
 	{
-		if (v[i] > new_val)
+		if (!cmp(new_val, v[i]))
 		{
-			//throw
+			// TODO throw an exception
 			return;
 		}
 		v[i] = new_val;
-		while (i >= 1 && v[parent(i + 1) - 1] < v[i])
+
+		while (i > 1 && !cmp(v[parent(i)], v[i]))
 		{
-			swap(v[i], v[parent(i + 1) - 1]);
-			i = parent(i + 1) - 1;
+			swap(v[i], v[parent(i)]);
+			i = parent(i);
 		}
 	}
+
 public:
-	heap() : v(), cmp(), heap_size(0) {}
-
-	heap(vector<value_type>& v_) : v(v_), heap_size(v_.size())
+	
+	heap() : v(vector<T>(1, T())), cmp(Compare()), heap_size(0), is_heap(false) {}
+	heap(const heap<T>& m2) : v(m2.v), cmp(m2.cmp), heap_size(m2.heap_size) { }
+	
+	heap(const vector<value_type>& v2) : heap()
 	{
-		_build_heap_();
+		for (const value_type& q : v2)
+		{
+			v.push_back(q);
+		}
+		build_heap();
 	}
 
-	heap(vector<value_type>&& v_) : v(move(v_)), heap_size(v_.size())
+	template <typename It>
+	heap(It begin, It end) : heap()
 	{
-		_build_heap_();
+		while (begin != end)
+		{
+			v.push_back(*begin++);
+		}
+		build_heap();
 	}
 
-	heap(const heap<T>& m2) : cmp(m2.cmp), v(m2.v), heap_size(m2.heap_size) { }
+	heap(heap<value_type>&& m2) : heap()
+	{
+		swap_(*this, m2);
+	}
 
 	heap<value_type>& operator=(const heap<value_type>& m2)
 	{
-		heap<value_type> d(m2);
-		swap_(*this, d);
-		return *this;
+		heap<T> h(m2);
+		swap_(*this, h);
 	}
 
-	heap<value_type>& operator=(heap<value_type>&& m2) noexcept
+	heap<value_type>& operator=(heap<value_type>&& m2)
 	{
 		swap_(*this, m2);
-		return *this;
 	}
 
-	template <typename Q>
-	friend void swap_(heap<Q>& m1, heap<Q>& m2)
+	friend void swap_(heap<value_type, Compare>& m1, heap<value_type, Compare>& m2)
 	{
-		std::swap(m1.cmp, m2.cmp);
 		std::swap(m1.v, m2.v);
+		std::swap(m1.cmp, m2.cmp);
 		std::swap(m1.heap_size, m2.heap_size);
 	}
 
+	value_type& operator[](size_t i) { return v[i]; }
+	const value_type& operator[](size_t i) const { return v[i]; }
+
 	template <typename Q>
-	friend ostream& operator<<(ostream& os, const heap<Q>& m)
+	friend ostream& operator<<(ostream& os, const heap<Q>& h)
 	{
-		copy(m.v.cbegin(), m.v.cbegin() + m.heap_size, ostream_iterator<Q>(os, " "));
+		copy(next(h.v.cbegin()), h.v.cbegin() + h.heap_size + 1, ostream_iterator<Q>(os, " "));
 		return os;
 	}
 
 	void heap_sort()
 	{
-	//	_build_heap_();
-		int i = heap_size - 1;
-		while (i > 0)
+	//	build_heap();
+		// TODO for min-heap
+		size_t hs = heap_size;
+		for (size_t i = heap_size; i >= 2; --i)
 		{
-			swap(v[i], v[0]);
+			swap(v[1], v[i]);
 			--heap_size;
-			_heapify_(1);
-			--i;
+			heapify(1);
 		}
+		heap_size = hs;
+		is_heap = false;
 	}
 
-	vector<int>& get_vector()
-	{
-		return v;
-	}
+	const size_t size() const { return heap_size; }
+//	const vector<value_type>& get_vector() const { return v; }
 };
 
-
-template <typename T, typename Cont, typename Cmp>
-class priority_queue_
-{
-	Cont cont;
-	Cmp cmp;
-public:
-	priority_queue_() : cont(), cmp() { }
-
-	template <typename InputIt>
-	priority_queue_(InputIt first, InputIt last, const Cmp& compare = Cmp{})
-	{
-		vector<T> v;
-		v.reserve(distance(first, last));
-
-		copy(first, last, back_inserter(v));
-		cont = move(v);
-	}
-
-	template <typename Q>
-	friend ostream& operator<<(ostream& os, const priority_queue_<Q>& m)
-	{
-		return os << m.cont << endl;
-	}
-
-	const T& top() const noexcept
-	{
-		return cont.v[0];
-	}
-
-	void push(const T& value)
-	{
-		cont.heap_size = cont.heap_size + 1;
-		cont.v[cont.heap_size - 1] = numeric_limits<T>::min();
-		cont._heap_increase_val_(cont.heap_size - 1, value);
-	}
-
-	T pop()
-	{
-		if (cont.heap_size < 1)
-		{
-			throw ("empty queue");
-		}
-		T max_ = cont.v[0];
-		cont.v[0] = cont.v[cont.heap_size - 1];
-		--cont.heap_size;
-		cont._heapify_(1);
-		return max_;
-	}
-
-	bool empty() const
-	{
-		return cont.heap_size == 0;
-	}
-};
 
 std::vector<int> generate_random_vector(size_t size, int left = -1000, int right = 1000)
 {
@@ -220,6 +190,34 @@ std::vector<int> generate_random_vector(size_t size, int left = -1000, int right
 	return data;
 }
 
+template <typename T, typename Compare = greater<T>>
+bool operator==(const heap<T, Compare>& m, const vector<T>& v)
+{
+	if (m.size() != v.size())
+		return false;
+
+	for (int i = 0; i < m.size(); ++i)
+	{
+		if (m[i+1] != v[i])
+			return false;
+	}
+	return true;
+}
+
+void make_heap_tests()
+{
+	for (int i = 0; i < 10; ++i)
+	{
+		vector<int> v = generate_random_vector(10000);
+	//	vector<int> v2(v);
+		heap<int> m(v);
+	//	make_heap(v2.begin(), v2.end());
+	//	cout << boolalpha << (m == v2) << endl; // QUESTION: WHY ? 
+		cout << boolalpha << is_heap(&m[1], &m[1] + m.size()) << endl;
+	}
+
+}
+
 void heap_sort_tests()
 {
 	vector<int> v1 = { 16,14,10,8,7,9,3,2,4,1 };
@@ -227,7 +225,7 @@ void heap_sort_tests()
 	vector<int> v3 = { 4,1,3,2,16,9,10,14,8,7 };
 
 	heap<int> m(v3);
-	//heap<int> m(v);
+
 	cout << m << endl;
 	m.heap_sort();
 	cout << m << endl;
@@ -236,31 +234,98 @@ void heap_sort_tests()
 	{
 		vector<int> v = generate_random_vector(10000);
 		vector<int> v2(v);
+
 		heap<int> m(v);
+
 		sort(v2.begin(), v2.end());
 		m.heap_sort();
-		v = m.get_vector();
-		cout << boolalpha << (v == v2) << endl;
+
+		cout << boolalpha << (m == v2) << endl;
 	}
 }
+
+template<class T, class Compare, class Container>
+class priority_queue_
+{
+	Container m;
+	using value_type = typename Container::value_type;
+public:
+	priority_queue_() : m() {}
+
+	template <typename It>
+	priority_queue_(It begin, It end) : m(begin, end) { }
+
+	const value_type& top() const
+	{
+		return m[1];
+	}
+	
+	void push(const value_type& value)
+	{
+		++m.heap_size;
+		m.v.push_back(numeric_limits<value_type>::min());
+		m.increase_val(m.heap_size, value);
+	}
+
+	template< class... Args >
+	void emplace(Args&&... args)
+	{
+
+	}
+
+	void pop()
+	{
+		if (m.heap_size == 0)
+		{
+			//throw an exception
+			return;
+		}
+		m[1] = m[m.heap_size];
+		--m.heap_size;
+		m.heapify(1);
+	}
+
+	const size_t empty() const { return m.heap_size == 0; }
+};
+
 
 void priority_queue_test()
 {
 	vector<int> v1 = { 4,1,3,2,16,9,10,14,8,7 };
 	priority_queue_<int> pq(v1.cbegin(), v1.cend());
-//	cout << pq << endl;
+	priority_queue_<int> pq2;
+	std::priority_queue<int> pq_std(v1.cbegin(), v1.cend());
 
-	heap<int> m(v1);
-	cout << m << endl;
+	for (int i : v1)
+	{
+		pq2.push(i);
+	}
 
+	bool b = true;
 	while (!pq.empty())
 	{
 		int c = pq.top();
 		pq.pop();
-		cout << "c = " << c << endl;
-//		cout << pq << endl;
+
+		int c2 = pq2.top();
+		pq2.pop();
+
+		int c3 = pq_std.top();
+		pq_std.pop();
+		
+		if (c != c3 || c2 != c3)
+		{
+			b = false;
+			cout << boolalpha << false << endl;
+			break;
+		}
+	}
+	if (b)
+	{
+		cout << boolalpha << true << endl;
 	}
 }
+
 
 void priority_queue_tests()
 {
@@ -268,27 +333,50 @@ void priority_queue_tests()
 	{
 		size_t sz = 10000;
 		vector<int> v = generate_random_vector(sz);
-		std::priority_queue<int> pq_std(v.cbegin(), v.cend());
-		priority_queue_<int> pq_my(v.cbegin(), v.cend());
 
+		priority_queue_<int> pq(v.cbegin(), v.cend());
+		priority_queue_<int> pq2;
+		std::priority_queue<int> pq_std(v.cbegin(), v.cend());
+
+		for (int i : v)
+		{
+			pq2.push(i);
+		}
+
+		bool b = true;
 		while (sz--)
 		{
-			int c_std = pq_std.top();
-			int c_my = pq_my.top();
+			int c = pq.top();
+			pq.pop();
 
-			if (c_std != c_my)
+			int c2 = pq2.top();
+			pq2.pop();
+
+			int c3 = pq_std.top();
+			pq_std.pop();
+
+			if (c != c3 || c2 != c3)
 			{
+				b = false;
 				cout << boolalpha << false << endl;
-				return;
+				break;
 			}
 		}
-		cout << boolalpha << true << endl;
+		if (b)
+		{
+			cout << boolalpha << true << endl;
+		}
 	}
 }
 
+
 int main()
 {
-	//heap_sort_tests();
+	make_heap_tests();
+	heap_sort_tests();
+
+	priority_queue_test();
+	
 	priority_queue_tests();
 	return 0;
 }
